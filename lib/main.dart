1109401932caa124189e2a174ded386401cc2957
nizarassegaf <nizar@gits.co.id>
2019-05-03 16:40:22 +0700

    first commit Hedda

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_sqflite/database_helper.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SQFliteDemo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final dbHelper = DatabaseHelper.instance;
+
+
+  Widget build(BuildContext contex){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('sqflite'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('insert', style: TextStyle(fontSize: 20),),
+              onPressed: () {
+                _insert();
+                },
+            ),
+            RaisedButton(
+              child: Text('query', style: TextStyle(fontSize: 20),),
+              onPressed: () {_query();},
+            ),
+            RaisedButton(
+              child: Text('update', style: TextStyle(fontSize: 20),),
+              onPressed: () {_update();},
+            ),
+            RaisedButton(
+              child: Text('delete', style: TextStyle(fontSize: 20),),
+              onPressed: () {_delete();},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Button onPressed methods
+
+  void _insert() async {
+    // row to insert
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnName : 'Bob',
+      DatabaseHelper.columnAge  : 23
+    };
+    final id = await dbHelper.insert(row);
+    print('inserted row id: $id');
+  }
+
+  void _count() async{
+    final count = await dbHelper.queryRowCount();
+
+  }
+
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows();
+//    if()
+    print('query all rows:');
+    allRows.forEach((row) => print(row));
+  }
+
+  void _update() async {
+    // row to update
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnID   : 1,
+      DatabaseHelper.columnName : 'Mary',
+      DatabaseHelper.columnAge  : 32
+    };
+    final rowsAffected = await dbHelper.update(row);
+    print('updated $rowsAffected row(s)');
+  }
+
+  void _delete() async {
+    // Assuming that the number of rows is the id for the last row.
+    final id = await dbHelper.queryRowCount();
+    if(id == 0){
+      print('Tidak ada DB');
+    } else {
+      final rowsDeleted = await dbHelper.delete(id);
+      print('deleted $rowsDeleted row(s): row $id');
+    }
+  }
+
+
+}
+
+
